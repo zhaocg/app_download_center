c79e91d1fbaf7195048235f25ce60c0703b5e2e9
zhaochenguang <zhaocgmail@163.com>
2025-12-20 22:56:22 +0800

    部署

--- lib/config.ts
+++ lib/config.ts
@@ -1,10 +1,10 @@
 import path from "node:path";
 
 export const DOWNLOAD_ROOT =
-  process.env.DOWNLOAD_ROOT || path.join(process.cwd(), "data/share/TRGE产品打包专用");
+  process.env.DOWNLOAD_ROOT || path.join(process.cwd(), "data/share/packages");
 
 export const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://192.168.0.141:27017/app-download-center";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/app-download-center";
 
 export const CLEANUP_TOKEN = process.env.CLEANUP_TOKEN || "";
 
